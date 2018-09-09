ad16223be1bb4b34fa33c3939c527618cb381988
Jill Bender <jbend@microsoft.com>
2018-09-09 15:55:49 -0700

    Add demo loop for truck 2

--- device-simulation/Services/data/devicemodels/scripts/truck-01-state.js
+++ device-simulation/Services/data/devicemodels/scripts/truck-01-state.js
@@ -7,14 +7,12 @@
 
 "use strict";
 
-var center_latitude = 47.445301;
-var center_longitude = -122.296307;
-
 // Default state
 var state = {
     online: true,
-    latitude: center_latitude,
-    longitude: center_longitude,
+    altitude: 83.0,
+    latitude: 47.50066160,
+    longitude: -122.1859029,
     speed: 80.0,
     speed_unit: "mph",
     temperature: 38.0,
@@ -22,7 +20,10 @@ var state = {
 };
 
 // Default properties
-var properties = {};
+var properties = {
+    latitude: 47.50066160,
+    longitude: -122.1859029
+};
 
 /**
  * Restore the global state using data from the previous iteration.
@@ -56,17 +57,55 @@ function vary(avg, percentage, min, max) {
     return value;
 }
 
+// Demo loop of data for truck
+// latitude, longitude, altitude, temperature 
+var data = [
+    [47.50066160, -122.1859029, 83.0, 14.004],
+    [47.50344484, -122.1768335, 113.0, 13.644],
+    [47.50530025, -122.1569494, 123.0, 13.524],
+    [47.50588005, -122.1413568, 136.0, 13.368],
+    [47.50240114, -122.1298555, 113.0, 13.644],
+    [47.49811017, -122.1157792, 108.0, 13.704],
+    [47.49845323, -122.0979256, 102.0, 13.776],
+    [47.51028147, -122.0863957, 125.0, 13.5],
+    [47.51631052, -122.0721478, 134.0, 13.392],
+    [47.52593239, -122.0644230, 69.0, 14.172],
+    [47.53636374, -122.0627064, 36.0, 14.568],
+    [47.54783583, -122.0613331, 16.0, 14.808],
+    [47.54575018, -122.0540089, 16.0, 14.808],
+    [47.54383827, -122.0431943, 20.0, 14.76],
+    [47.53647963, -122.0326371, 29.0, 14.652],
+    [47.53149603, -122.0232672, 49.0, 14.412],
+    [47.53636374, -122.0088477, 94.0, 13.872],
+    [47.53253915, -121.9981188, 130.0, 13.44],
+    [47.53068471, -121.9867034, 150.0, 13.2],
+    [47.53549454, -121.9714112, 164.0, 13.032],
+    [47.53242325, -121.9557042, 151.0, 13.188],
+    [47.52813473, -121.9404263, 164.0, 13.032],
+    [47.51944072, -121.9279809, 140.0, 13.32],
+    [47.50968239, -121.9089264, 199.0, 12.612],
+    [47.50713140, -121.8903727, 276.0, 11.688],
+    [47.50736331, -121.8837638, 277.0, 11.676],
+    [47.51080999, -121.8561367, 282.0, 11.616],
+    [47.50489626, -121.8252377, 194.0, 12.672]
+];
+
 /**
- * Generate a random geolocation at some distance (in miles)
- * from a given location
+ * Method used by findIndex to locate the index of the current
+ * location by latitude
  */
-function varylocation(latitude, longitude, distance) {
-    // Convert to meters, use Earth radius, convert to radians
-    var radians = (distance * 1609.344 / 6378137) * (180 / Math.PI);
-    return {
-        latitude: latitude + radians,
-        longitude: longitude + radians / Math.cos(latitude * Math.PI / 180)
-    };
+function findLatitude(location) {
+    return location[0] === properties.latitude;
+}
+
+/**
+ * Returns the next data point in the predefined data set.
+ * Loops if the end of the list has been reached.
+ */
+function getNextMessage() {
+    var index = data.findIndex(findLatitude);
+    if (index === data.length - 1) return data[0];
+    return data[index + 1];
 }
 
 /**
@@ -86,16 +125,21 @@ function main(context, previousState, previousProperties) {
     // using the previous function state.
     restoreSimulation(previousState, previousProperties);
 
-    // 0.1 miles around some location
-    var coords = varylocation(center_latitude, center_longitude, 0.1);
-    state.latitude = coords.latitude;
-    state.longitude = coords.longitude;
+    // Get the next data point in the demo loop
+    // stored as an array of [latitude, longitude, altitude, temperature]
+    var data = getNextMessage();
+    state.latitude = properties.latitude = data[0];
+    state.longitude = properties.longitude = data[1];
+    state.altitude = data[2];
+    state.temperature = data[3];
 
     // 30 +/- 5%,  Min 0, Max 80
     state.speed = vary(30, 5, 0, 80);
 
-    // current temperature +/- 1%,  Min/Max: current +/- 15
-    state.temperature = vary(state.temperature, 1, state.temperature - 15, state.temperature + 15);
-
     updateState(state);
+    updateProperty("latitude", state.latitude);
+    updateProperty("longitude", state.longitude);
+
+    // Sleep so truck movement seems more realistic
+    sleep(20000);
 }
--- device-simulation/Services/data/devicemodels/scripts/truck-02-state.js
+++ device-simulation/Services/data/devicemodels/scripts/truck-02-state.js
@@ -7,14 +7,12 @@
 
 "use strict";
 
-var center_latitude = 47.25433;
-var center_longitude = -121.177075;
-
 // Default state
 var state = {
     online: true,
-    latitude: center_latitude,
-    longitude: center_longitude,
+    altitude: 83.0,
+    latitude: 47.5006616,
+    longitude: -122.1859029,
     speed: 80.0,
     speed_unit: "mph",
     temperature: 49.0,
@@ -22,7 +20,10 @@ var state = {
 };
 
 // Default properties
-var properties = {};
+var properties = {
+    latitude: 47.5006616,
+    longitude: -122.1859029
+};
 
 /**
  * Restore the global state using data from the previous iteration.
@@ -69,6 +70,57 @@ function varylocation(latitude, longitude, distance) {
     };
 }
 
+// Demo loop of data for truck
+// latitude, longitude, altitude, temperature 
+var data = [
+    [47.50066160, -122.1859029, 83.0, 14.004],
+    [47.50344484, -122.1768335, 113.0, 13.644],
+    [47.50530025, -122.1569494, 123.0, 13.524],
+    [47.50588005, -122.1413568, 136.0, 13.368],
+    [47.50240114, -122.1298555, 113.0, 13.644],
+    [47.49811017, -122.1157792, 108.0, 13.704],
+    [47.49845323, -122.0979256, 102.0, 13.776],
+    [47.49437276, -122.0852188, 108.0, 13.704],
+    [47.48892117, -122.0773224, 105.0, 13.74],
+    [47.48114878, -122.0603279, 102.0, 13.776],
+    [47.47906048, -122.0392136, 81.0, 14.028],
+    [47.48474510, -122.0271973, 62.0, 14.256],
+    [47.47221492, -122.0233349, 101.0, 13.788],
+    [47.46119052, -122.0099167, 95.0, 13.86],
+    [47.45260149, -121.9951538, 118.0, 13.584],
+    [47.44168908, -121.9850258, 132.0, 13.416],
+    [47.43541923, -121.9754128, 156.0, 13.128],
+    [47.44849993, -121.9585900, 270.0, 11.76],
+    [47.46019325, -121.9474034, 377.0, 10.476],
+    [47.46529967, -121.9345001, 406.0, 10.128],
+    [47.46557599, -121.9265687, 413.0, 10.044],
+    [47.47075370, -121.9128708, 347.0, 10.836],
+    [47.47353853, -121.8981079, 271.0, 11.748],
+    [47.48370961, -121.8891243, 234.0, 12.192],
+    [47.49658500, -121.8860344, 280.0, 11.64],
+    [47.50736331, -121.8837638, 277.0, 11.676],
+    [47.51080999, -121.8561367, 282.0, 11.616],
+    [47.50489626, -121.8252377, 194.0, 12.672]
+];
+
+/**
+ * Method used by findIndex to locate the index of the current
+ * location by latitude
+ */
+function findLatitude(location) {
+    return location[0] === properties.latitude;
+}
+
+/**
+ * Returns the next data point in the predefined data set.
+ * Loops if the end of the list has been reached.
+ */
+function getNextMessage() {
+    var index = data.findIndex(findLatitude);
+    if (index === data.length - 1) return data[0];
+    return data[index + 1];
+}
+
 /**
  * Entry point function called by the simulation engine.
  * Returns updated simulation state.
@@ -86,10 +138,13 @@ function main(context, previousState, previousProperties) {
     // using the previous function state.
     restoreSimulation(previousState, previousProperties);
 
-    // 0.1 miles around some location
-    var coords = varylocation(center_latitude, center_longitude, 0.1);
-    state.latitude = coords.latitude;
-    state.longitude = coords.longitude;
+    // Get the next data point in the demo loop
+    // stored as an array of [latitude, longitude, altitude, temperature]
+    var data = getNextMessage();
+    state.latitude = properties.latitude = data[0];
+    state.longitude = properties.longitude = data[1];
+    state.altitude = data[2];
+    state.temperature = data[3];
 
     // 30 +/- 5%,  Min 0, Max 80
     state.speed = vary(30, 5, 0, 80);
@@ -98,4 +153,9 @@ function main(context, previousState, previousProperties) {
     state.temperature = vary(state.temperature, 1, state.temperature - 15, state.temperature + 15);
 
     updateState(state);
+    updateProperty("latitude", state.latitude);
+    updateProperty("longitude", state.longitude);
+
+    // Sleep so truck movement seems more realistic
+    sleep(20000);
 }
